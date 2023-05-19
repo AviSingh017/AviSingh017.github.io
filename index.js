f2aa8a03a57711da1dc77c0fa6249c9efaafb63b
Avishek Singh <115368094+AviSingh017@users.noreply.github.com>
2023-05-19 05:33:21 +0530

    Created Home About Projects & Contact sections 02

--- index.js
+++ index.js
@@ -1,67 +1,114 @@
-/////////////////////////////////////////// Toggle icon navbar/////////////////////////
+var successSound = document.getElementById("success-sound");
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('fa-x');
-    navbar.classList.toggle('active');
-};
-
-/////////////////////////////////////////// scroll section active links/////////////////////////
-
-let section = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    section.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            });
-        };
+document.getElementById("resume-link-1").addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1riA923Sppz31hfNDHd_VumC_tHHHc5hI/view?usp=share_link"
+    );
+  });
+  
+  document.getElementById("resume-link-2").addEventListener("click", () => {
+    window.open(
+      "https://drive.google.com/file/d/1riA923Sppz31hfNDHd_VumC_tHHHc5hI/view?usp=share_link"
+      );
     });
-
-    /////////////////////////////////////////// Sticky navbar/////////////////////////
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    /////////////////////////remove toggle icon and navbar when click navbar link (scroll)/////////////////////
-
-    // menuIcon.classList.toggle('fa-x');
-    // navbar.classList.toggle('active');
-
+    
+    let details = document.getElementsByClassName("skill-ed");
+    let content = document.getElementsByClassName("contents");
+    
+opentab = (name) => {
+  for (detail of details) {
+    detail.classList.remove("active");
+  }
+  for (con of content) {
+    con.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active");
+  document.getElementById(name).classList.add("active-tab");
 };
 
-/////////////////////////////////////////// Scroll Reveal/////////////////////////
+let sidemenu = document.getElementById("sidemenu");
 
-ScrollReveal({ 
-    reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
- });
+openmenu = () => {
+  sidemenu.style.right = "0";
+};
 
- ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
- ScrollReveal().reveal('.home-img, .portfolio-box, contact form', { origin: 'bottom' });
- ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
- ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+closemenu = () => {
+  sidemenu.style.right = "-200px";
+};
 
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbx82hI_piwkiQD_at1DeSyXlBLq53Twggg1suj3j8zfUQryozddn4zZHOQb1bYPbQfs/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      successSound.play();
+      swal("", "Message Sent Successfully", "success");
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
 
- ////////////////////////////////////////// typed js////////////////////////////////////
+window.addEventListener("scroll", function () {
+  let nav = this.document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
 
+let container = document.getElementById("skill_container");
 
-const typed = new Typed('.multiple-text',{
-    strings: ['Full Stack Developer'],
-    typeSpeed: 70,
-    backSpeed: 70,
-    backDelay: 1000,
-    loop: true 
+let skill_arr = [
+  "./images/html.png",
+  "./images/css.png",
+  "./images/js.png",
+  "./images/nodejslogo.png",
+  "./images/express.png",
+  "./images/mongoose.png",
+  "./images/mongodb.png",
+  "./images/typescript.png",
+  "./images/sql.jpg",
+  "./images/redis.png",
+  "./images/aws.png",
+  "./images/git.png",
+];
+let skill_name = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Node.JS",
+  "Express",
+  "Mongoose",
+  "MongoDB",
+  "TypeScript",
+  "SQL",
+  "Redis",
+  "AWS",
+  "GIT",
+];
+let count = 0;
+AppendSkills = (arr) => {
+  container.innerHTML = null;
+  arr.forEach((ele) => {
+    let div = document.createElement("div");
+
+    div.classList.add("skills-card");
+    
+    let img = document.createElement("img");
+    img.src = ele;
+    img.classList.add("skills-card-img");
+    
+    let name = document.createElement("h2");
+    name.classList.add("skills-card-name");
+
+    name.innerText = skill_name[count];
+    name.style.color = "white";
+    count++;
+    div.append(img, name);
+    container.append(div);
+  });
+};
+
+window.addEventListener("load", () => {
+  AppendSkills(skill_arr);
 });
